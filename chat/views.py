0a0c972ccf4bba7ed462be910b0340f51cc9d22c
Daniel Tesfai <dannyhd88@gmail.com>
2022-10-04 14:57:14 +0300

    after model decision

--- chat/views.py
+++ chat/views.py
@@ -1,46 +1,55 @@
-from mailbox import Message
-
-from django.shortcuts import get_object_or_404
-from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
 
-from chat.models import Conversation, Message, User
-from chat.serializers import (ConversationSerializer, MessageSerializer,
-                              UserSerializer)
+from .paginator import MessagePagination
+from rest_framework.response import Response
 
+from .models import Conversation, Message,User
+from .serializers import ConversationSerializer, MessageSerializer,UserSerializer
 
-class ConversationViewset(ModelViewSet):
-    serializer_class = ConversationSerializer
-    queryset = Conversation.objects.none()
-
-    def get_queryset(self):
-        queryset = Conversation.objects.all()
-        username = self.request.query_params.get('username', None)
-        if username is not None:
-            contact = get_user_contact(username)
-            queryset = contact.Conversation.all()
-        return queryset
 
 class UserViewset(ModelViewSet):
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-class MessageViewset(ModelViewSet):
-    queryset=Message.objects.all()
-    serializer_class=MessageSerializer
-
-
-    
-
-def get_last_10_messages(chatId):
-    chat = get_object_or_404(Conversation, id=chatId)
-    return chat.messages.order_by('-timestamp').all()[:10]
+    def list(self, request, *args, **kwargs):
+        user = self.request.query_params.get('user')
+        if user is not None:
+            queryset = User.objects.filter(
+                UserId=user).values()
+            return Response(queryset)
+        else:
+            return Response(User.objects.all().values())
 
 
-def get_user_contact(username):
-    return get_object_or_404(User, UserId=username)
-    #return get_object_or_404(Contact, user=user)
+class ConversationViewSet(ModelViewSet):
+    serializer_class = ConversationSerializer
+    queryset = Conversation.objects.none()
+    lookup_field = "UserId"
+
+    def get_queryset(self):
+        queryset = Conversation.objects.filter(
+            UserId__contains=self.request['UserId']
+        )
+        return queryset
+
+    def get_serializer_context(self):
+        return {"request": self.request, "user": self.request['UserId']}
 
 
-def get_current_chat(chatId):
-    return get_object_or_404(Conversation, id=chatId)
+
+
+class MessageViewSet(ModelViewSet):
+    serializer_class = MessageSerializer
+    queryset = Message.objects.none()
+    pagination_class = MessagePagination
+
+    def get_queryset(self):
+        conversation_name = self.request.GET.get("conversation")
+        queryset = (
+            Message.objects.filter(
+                conversation__name__contains=self.request.user.username,
+            )
+            .filter(conversation__name=conversation_name)
+            .order_by("-timestamp")
+        )
+        return queryset
--- core/routing.py
+++ core/routing.py
@@ -11,11 +11,11 @@
 # })
 from django.urls import path
 
-from chat.consumers import ChatConsumer
+from chat.consumers import ChatConsumer, NotificationConsumer,NotificationConsumer
 
 websocket_urlpatterns = [ 
     path("chats/<conversation_name>/", ChatConsumer.as_asgi()),
-    #path("notifications/", NotificationConsumer.as_asgi()),
+    path("notifications/", NotificationConsumer.as_asgi()),
     
     ]
     
